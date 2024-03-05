8508d46306c645e7bc80b7e4ab0181fbba651936
truong <truong263111@gmail.com>
2024-03-05 21:37:06 +0700

    bat dau voi php va bootstrap

--- pages/frmdangki.php
+++ pages/frmdangki.php
@@ -1,15 +1,39 @@
 <div id="frmdangki">
-    <form class="container bg-warning d-grid" action="./pages/xulydangki.php" onsubmit="return validate()">
-        <label class="row-*-*">maKH <input type="text" name="makh"></label>
-        <label class="row-*-*">Ho ten<input type="text" name="hoten"></label>
-        <label class="row-*-*">Email <input type="text" name="email"></label>
-        <label class="row-*-*">Dien thoai <input type="text" name="dt"></label>
-        <label class="row-*-*">Dia chi <input type="text" name="dchi"></label>
-        <input type="submit" value="Dang ki"> 
+    <form class="form-horizontal row bg-white">
+        <div class="h2 text-center pt-3">Form đăng kí</div>
+        <div class="form-group">
+            <label for="exampleFormControlInput1" class="">Email address</label>
+            <input type="email" class="form-control " id="exampleFormControlInput1" placeholder="name@example.com">
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlSelect1">Example select</label>
+            <select class="form-control" id="exampleFormControlSelect1">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlSelect2">Example multiple select</label>
+            <select multiple class="form-control" id="exampleFormControlSelect2">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlTextarea1">Example textarea</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
     </form>
 </div>
 
 <script>
     let dtregex = /^0\d{8}$/;
-    let emailregex = 
+    let emailregex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 </script>
